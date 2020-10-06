b1957f4213e2c733c0ed9bc4374c62636737dfe3
Gomolemo Mohapi <gomomohapi@gmail.com>
2020-10-06 14:39:43 +0200

    update User Controller

--- CubeX/Controllers/UserController.cs
+++ CubeX/Controllers/UserController.cs
@@ -3,11 +3,13 @@ using CubeX.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
 namespace CubeX.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class UserController : Controller
     {
 
@@ -36,6 +38,111 @@ namespace CubeX.Controllers
             var usersList = user.ToList();
 
             return View(usersList);
+        }
+
+        //GET Edit
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationUser user = db.Users.Find(id);
+
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            UserVM model = new UserVM()
+            {
+                FirstName = user.FirstName,
+                Surname = user.Surname,
+                Email = user.Email,
+                Id = user.Id
+            };
+
+            return View(model);
+        }
+
+        //POST Method for EDIT Action
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(UserVM user)
+        {
+            if (!ModelState.IsValid)
+            {
+                UserVM model = new UserVM()
+                {
+                    FirstName = user.FirstName,
+                    Surname = user.Surname,
+                    Email = user.Email,
+                    Id = user.Id
+                };
+                return View(model);
+            }
+            else
+            {
+                var userInDb = db.Users.Single(u => u.Id == user.Id);
+                userInDb.FirstName = user.FirstName;
+                userInDb.Surname = user.Surname;
+                userInDb.Email = user.Email;
+            }
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "User");
+        }
+
+
+        public ActionResult Details(string id)
+        {
+            if (id == null || id.Length == 0)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationUser user = db.Users.Find(id);
+            UserVM model = new UserVM()
+            {
+                FirstName = user.FirstName,
+                Surname = user.Surname,
+                Email = user.Email,
+                Id = user.Id
+            };
+            return View(model);
+        }
+
+        //DELETE Get Method
+        public ActionResult Delete(string id)
+        {
+            if (id == null || id.Length == 0)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationUser user = db.Users.Find(id);
+            UserVM model = new UserVM()
+            {
+                FirstName = user.FirstName,
+                Surname = user.Surname,
+                Email = user.Email,
+                Id = user.Id
+            };
+            return View(model);
+        }
+
+        //DELETE Post method
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            var userInDb = db.Users.Find(id);
+            if (id == null || id.Length == 0)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            //userInDb.Disable = true;
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
